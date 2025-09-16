3a36b14c1bb693d63f3849e62f968fd9148b7568
Soumyaranjan Mahalik <124546914+srm1993@users.noreply.github.com>
2025-09-16 21:01:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app=express();
 const path=require('path');
-const routes=require('./routes/router');
+const routes=require('./routes/router.js');
 const mongoose = require('mongoose');
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
