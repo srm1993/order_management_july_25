aa3228756c1e0e64989083960d294f73def79848
Soumyaranjan Mahalik <124546914+srm1993@users.noreply.github.com>
2025-09-16 20:32:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const routes=require('./routes/router');
 const mongoose = require('mongoose');
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-mongoose.connect('mongodb://localhost:27017/ordermanagement_july25')
+mongoose.connect('mongodb+srv://soumyamahalik:kadali90@cluster0.a4jtujh.mongodb.net/ordermanagement_july25')
 .then(()=>console.log('MongoDB connected'))
 .catch((err)=>console.log(err));
 app.set('view engine','ejs');
